5ddfa0e349c08fdaefb3597166d183a33ffd4419
itggot-axel-vallin <axel.vallin@elev.ga.ntig.se>
2019-03-14 15:40:46 +0100

    Layouten är klar för main sidan

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,9 @@
 function showHamburger(event){
     let menu = document.querySelector("nav")
     menu.classList.toggle("navhidden")
+}
 
-    
+function showCategories(event){
+    let menu = document.querySelector(".categorieshidden")
+    menu.classList.toggle("categoriesshow")
 }
