c668798ec67fa4bfdc09386d6f266e3b6b7c2727
aCmEaw <axel.wallin01@gmail.com>
2019-03-29 22:23:35 +0100

    Gjorde klart :)

--- site/js/main.js
+++ site/js/main.js
@@ -1,6 +1,9 @@
 function showHamburger(event){
     let menu = document.querySelector("nav")
     menu.classList.toggle("navhidden")
+
+    let fadeLayer = document.querySelector(".fade-layer")
+    fadeLayer.classList.toggle("visible")
 }
 
 function showCategories(event){
